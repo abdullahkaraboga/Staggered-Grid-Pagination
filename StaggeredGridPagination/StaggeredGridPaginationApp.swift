f22350fe69f6ca8577d455f139254e86c0dcfd15
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-01-18 23:24:09 +0300

    Initial Commit

---
+++ StaggeredGridPagination/StaggeredGridPaginationApp.swift
@@ -0,0 +1,20 @@
+//
+//  StaggeredGridPaginationApp.swift
+//  StaggeredGridPagination
+//
+//  Created by Abdullah Karaboğa on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct StaggeredGridPaginationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
