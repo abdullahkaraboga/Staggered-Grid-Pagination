8c342d853902eb3d2efec706be4e51802f975d36
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-01-21 00:05:39 +0300

    json parsing process

--- StaggeredGridPagination/ContentView.swift
+++ StaggeredGridPagination/ContentView.swift
@@ -6,34 +6,60 @@
 //
 
 import SwiftUI
-import CoreData
+import SDWebImageSwiftUI
 
 struct ContentView: View {
 
-
+    @StateObject var imageFetcher: ImageFetcher = .init()
     var body: some View {
-        NavigationView {
-            ScrollView {
-                GridView(items: 1...23, id: \.self) { item in
-                    ZStack {
-                        Rectangle()
-                            .fill(.cyan)
 
-                        Text("\(item)")
-                            .font(.title.bold())
+        NavigationView {
+            Group {
+                if let images = imageFetcher.fetchedImages {
+                    ScrollView {
+                        CustomGridView(items: images, id: \.id) { item in
+                            GeometryReader { proxy in
+                                let size = proxy.size
+                                WebImage(url: URL(string: item.download_url))
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: size.width, height: size.height)
+                                    .cornerRadius(10)
+                                    .onAppear {
+                                    if images.last?.id == item.id {
+                                        imageFetcher.startPagination = true
+                                    }
+                                }
+
+                            }
+                        }
+                    }.padding()
+                        .padding(.bottom, 10)
+
+                    if imageFetcher.startPagination && !imageFetcher.endPagination {
+                        ProgressView()
+                            .offset(y: -15)
+                            .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                                imageFetcher.updateImages()
+                            }
+                        }
                     }
+
+                } else {
+                    ProgressView()
                 }
-                .padding()
             }
                 .navigationTitle("Grid View")
         }
 
     }
+}
 
 
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
+
